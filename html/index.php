7cafbb0dfb2647437c1d6d73648a370404bfacbf
OlesYudin <oles.yudin2001@gmail.com>
2022-01-12 19:33:40 +0200

    Modify .php files

--- html/index.php
+++ html/index.php
@@ -6,33 +6,34 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon" />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"/>
-		<link rel="stylesheet" href="style.css" />
+		<link rel="stylesheet" href="css/style.css" />
 		<title>PRNG for password</title>
 	</head>
 	<body>
 		<header>
 			<div class="title">
-				<h1>Генерація псевдо випадкових криптостійких паролів</h1>
+				<h1>Генерація псевдо випадкових паролів</h1>
 				<p>
-					Генерація псевдо випадкових криптостійких паролів на основі ентропії
+					Генерація псевдо випадкових паролів на основі ентропії
 					операційної системи на базі ядра Linux
 				</p>
 			</div>
 		</header>
+		<!-- Request new password -->
 		<div class="form-output">
 			<form action="" method="POST">
 				<input type="submit" class="btn btn-success" value="Отримати пароль адміністратора" name="get_passwd" 	id="getPasswd"/>
 			</form>
 		</div>
+		<!-- Show new password -->
 		<div class="password-block">
 			<p>Новий пароль:</p>		
 			<?php include "get_password.php";?>
 		</div>
+		<!-- Get IP of Instance -->
 		<div class="ip-block">
 			<p>IP-addres:</p>		
-			<?php echo $_SERVER['SERVER_ADDR']; ?>
+			<?php shell_exec('sh sh_script/get_ip.sh'); ?>
 		</div>
-
-		
 	</body>
 </html>
