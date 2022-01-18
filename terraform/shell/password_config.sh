ab1dddcccb15d77791e5ca94b2ab9dfa29b762fd
OlesYudin <oles.yudin2001@gmail.com>
2022-01-18 19:12:08 +0200

    Edit comments

--- terraform/shell/password_config.sh
+++ terraform/shell/password_config.sh
@@ -5,6 +5,6 @@ sudo apt install -y docker.io
 # Add ubuntu to docker group
 sudo usermod -aG docker,root ubuntu
 mkdir ~/docker
-# sudo scp -i ~/.ssh/password-generator.pem -r /home/student/.docker/docker-password.txt ubuntu@18.222.142.81:/home/ubuntu/docker/
 # Its not work if you havent attached IP
+# sudo scp -i ~/.ssh/password-generator.pem -r /home/student/.docker/docker-password.txt ubuntu@18.222.142.81:/home/ubuntu/docker/
 sudo reboot -h now
