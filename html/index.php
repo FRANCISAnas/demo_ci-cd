6b19896de081b52e635d42825a043409a3d54779
OlesYudin <oles.yudin2001@gmail.com>
2022-01-18 12:34:06 +0200

    Remove Test

--- html/index.php
+++ html/index.php
@@ -12,7 +12,7 @@
 	<body>
 		<header>
 			<div class="title">
-				<h1>Generation of pseudo-random passwords! <span style="color:red">TEST</span></h1>
+				<h1>Generation of pseudo-random passwords!</h1>
 				<p>
 				Generation of pseudo-random passwords based on entropy operating system based on the Linux kernel
 				</p>
