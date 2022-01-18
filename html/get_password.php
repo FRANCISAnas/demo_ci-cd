8a9a7ce4745166ec3290c6c6010a89c07994d701
OlesYudin <oles.yudin2001@gmail.com>
2022-01-18 22:31:54 +0200

    Remove dir html from alpine image

--- html/get_password.php
+++
@@ -1,8 +0,0 @@
-<?php   
-    // $script = shell_exec("./script.sh");
-    if( isset( $_POST['get_passwd'] ) )
-    {       
-        $script = shell_exec('sh sh_script/script.sh');
-	    echo "<pre>$script</pre>";
-    }
-?>
--- html/sh_script/script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-tr -cd 'A-Za-z0-9-@#_?' < /dev/urandom | head -c12 ; echo
