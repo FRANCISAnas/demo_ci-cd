38630f88deb5d01beddb78b4a27aa681cd3ce5e4
OlesYudin <oles.yudin2001@gmail.com>
2022-01-26 19:56:44 +0200

    Update docker execution on instance

--- terraform_VPC/shell/password_config.sh
+++ terraform_VPC/shell/password_config.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 sudo apt update && sudo apt -y upgrade
+# Install docker
 sudo apt install -y docker.io
-# Add ubuntu to docker group
-sudo usermod -aG docker,root ubuntu
-mkdir ~/docker
-
-# Its not work if you havent attached IP
-# sudo scp -i ~/.ssh/password-generator.pem -r /home/student/.docker/docker-password.txt ubuntu@18.222.142.81:/home/ubuntu/docker/
-sudo reboot -h now
+# Execute container
+sudo docker pull olesyudin/password-generator:53
+sudo docker run -d -p 80:80 --name=password-generator olesyudin/password-generator:53
