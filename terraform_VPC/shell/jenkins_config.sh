23a5b75c426fb7beb4362fa104b813a06e865d27
OlesYudin <oles.yudin2001@gmail.com>
2022-01-26 23:30:55 +0200

    Delete old infra. Prepare repository for module Infrastructure

--- terraform_VPC/shell/jenkins_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt update && sudo apt -y upgrade
-sudo apt install -y docker.io
-# Add ubuntu to docker group
-sudo usermod -aG docker,root ubuntu
-mkdir ~/docker
-sudo docker pull jenkins/jenkins:2.330-alpine
-sudo docker run -d -p 8080:8080 --name=jenkins jenkins/jenkins:2.330-alpine
